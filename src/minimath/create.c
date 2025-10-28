3f4612eb38dbeaa22d814b6bb20f228e2001c797
aimonkied <simon.wied2000@gmail.com>
2025-10-28 01:41:21 +0100

    FIX(): Added {} to functions, so it compiles on all linux workmachines

--- src/minimath/create.c
+++ src/minimath/create.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   create.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fredchar <fredchar@student.42heilbronn.    +#+  +:+       +#+        */
+/*   By: swied <swied@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/15 14:54:23 by fredchar          #+#    #+#             */
-/*   Updated: 2025/10/24 16:56:27 by fredchar         ###   ########.fr       */
+/*   Updated: 2025/10/28 01:38:38 by swied            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,10 +29,10 @@ inline t_vec	tuple(double x, double y, double z, double w)
 
 inline t_mat	mat(const t_vec col0, const t_vec col1, const t_vec col2, const t_vec col3)
 {
-	return((t_mat){col0, col1, col2, col3});
+	return((t_mat){{col0, col1, col2, col3}});
 }
 
 inline t_mat	mat_idt()
 {
-	return((t_mat){vector(1, 0, 0), vector(0, 1, 0), vector(0, 0, 1), point(0, 0, 0)});
+	return((t_mat){{vector(1, 0, 0), vector(0, 1, 0), vector(0, 0, 1), point(0, 0, 0)}});
 }
