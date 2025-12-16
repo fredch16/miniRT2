624b79cc386ced45fac791286cacf07e5aaa7975
Frederick Charbonnier <frederickcharbonnier@icloud.com>
2025-12-16 18:56:19 +0100

    NORM - Fully conformed camera.c

--- src/minimath/camera.c
+++ src/minimath/camera.c
@@ -3,34 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   camera.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: swied <swied@student.42heilbronn.de>       +#+  +:+       +#+        */
+/*   By: fredchar <fredchar@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/02 19:26:23 by swied             #+#    #+#             */
-/*   Updated: 2025/11/02 21:00:18 by swied            ###   ########.fr       */
+/*   Updated: 2025/12/16 18:55:52 by fredchar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/miniRT.h"
 
-t_mat   view_transform(t_vec from, t_vec to, t_vec up)
+t_mat	view_transform(t_vec from, t_vec to, t_vec up)
 {
-    t_vec   forward;
-    t_vec   upn;
-    t_vec   left;
-    t_vec   true_up;
-    t_mat   orientation;
-    
-    forward = tuple_norm(tuple_sub(to, from));
-    upn = tuple_norm(up);
-    left = tuple_cro(forward, upn);
-    true_up = tuple_cro(left, forward);
-    orientation = mat(
-        tuple(left.x, left.y, left.z, 0),
-        tuple(true_up.x, true_up.y, true_up.z, 0),
-        tuple(-forward.x, -forward.y, -forward.z, 0),
-        tuple(0, 0, 0, 1)
-    );
-    return (mat_mul_mat(orientation, translation(-from.x, -from.y, -from.z)));
+	t_vec	forward;
+	t_vec	upn;
+	t_vec	left;
+	t_vec	true_up;
+	t_mat	orientation;
+
+	forward = tuple_norm(tuple_sub(to, from));
+	upn = tuple_norm(up);
+	left = tuple_cro(forward, upn);
+	true_up = tuple_cro(left, forward);
+	orientation = mat(
+			tuple(left.x, left.y, left.z, 0),
+			tuple(true_up.x, true_up.y, true_up.z, 0),
+			tuple(-forward.x, -forward.y, -forward.z, 0),
+			tuple(0, 0, 0, 1)
+			);
+	return (mat_mul_mat(orientation, translation(-from.x, -from.y, -from.z)));
 }
 
 t_camera	camera(int hsize, int vsize, double field_of_view)
