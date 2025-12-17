8f6c11f77953e1cdd0a0573e89dde3442a260ec6
Frederick Charbonnier <frederickcharbonnier@icloud.com>
2025-12-17 19:24:04 +0100

    NORM - Fully conformed mat_matmul.c

--- src/minimath/matmul.c
+++ src/minimath/matmul.c
@@ -6,21 +6,20 @@
 /*   By: fredchar <fredchar@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 15:16:16 by fredchar          #+#    #+#             */
-/*   Updated: 2025/10/24 17:03:32 by fredchar         ###   ########.fr       */
+/*   Updated: 2025/12/17 19:23:55 by fredchar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/miniRT.h"
 
-
 inline t_vec	mat_mul_vec(const t_mat m, const t_vec v)
 {
-	return (t_vec){
+	return ((t_vec){
 		m.c[0].x * v.x + m.c[1].x * v.y + m.c[2].x * v.z + m.c[3].x * v.w,
 		m.c[0].y * v.x + m.c[1].y * v.y + m.c[2].y * v.z + m.c[3].y * v.w,
 		m.c[0].z * v.x + m.c[1].z * v.y + m.c[2].z * v.z + m.c[3].z * v.w,
 		m.c[0].w * v.x + m.c[1].w * v.y + m.c[2].w * v.z + m.c[3].w * v.w
-	};
+	});
 }
 
 inline t_mat	mat_mul_mat(const t_mat a, const t_mat b)
